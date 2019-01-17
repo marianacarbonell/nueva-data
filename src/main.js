3dee64f33ee8aae83463771fc32f31eb8c8f2866
marianacarbonell <marianacarbonellm@gmail.com>
2019-01-17 17:03:39 -0300

    ya filtra y la barra está en el top pero no es fixed

--- src/main.js
+++ src/main.js
@@ -1,15 +1,13 @@
-const data =POKEMON.pokemon;
-const selectPokemon = document.getElementById("select");
-
+const data =(POKEMON.pokemon);
+const rootPokemon = document.getElementById("root");
+const selectType = document.getElementById("select"); 
 
 //mostrando tarjetas
-let pokemonCards = "";
-let containerRoot = document.getElementById("root");
-containerRoot.innerHTML="";
+const showData = (data) =>{
+let pokemonCards = " ";
+
 data.forEach(element => {
-    pokemonCards +=
-    
-    `
+  pokemonCards = rootPokemon.innerHTML += `
     <div class="col-sm-12 col-md-3 col-xl-3 col-lg-4">
     <div class="card" >
     <img src="${element.img}" class="card-img-top" alt="...">
@@ -21,8 +19,41 @@ data.forEach(element => {
     </div>
     </div>
     `
+}
+); 
+return pokemonCards;
+}
+
+
+// funcion filtro por tipo
+selectType.addEventListener("change",() => {
+let condition = selectType.value;
+let filterPokemon = filterType(data, condition);
+
+//para limpiar el div select
+rootPokemon.innerHTML = "";
+
+filterPokemon.forEach(element =>{
+  //if (element.weaknesses[1] === '' && element.type[1] === ''){
+    rootPokemon.innerHTML +=  `
+    <div class="col-sm-12 col-md-3 col-xl-3 col-lg-4">
+    <div class="card" >
+    <img src="${element.img}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <p class="card-text">${element.name}</p>
+      <p class="card-text">${element.num}</p>
+      <p class="card-text"><b>Tipos:</b> ${element.type.join(", ")}</p>
+      <p class="card-text"><b>Debilidades</b>: ${element.weaknesses.join(', ')}</p>
+    </div>
+    </div>
+    </div>
+    `
+}
+  
+ )
 });
-containerRoot.innerHTML = pokemonCards
+window.onload = showData(data);
+//rootPokemon.innerHTML = pokemonCards
 
 
 
