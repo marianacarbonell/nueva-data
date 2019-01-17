b2eb6782efb078cd79e0db6d2602882ba64cf28b
marianacarbonell <marianacarbonellm@gmail.com>
2019-01-17 17:03:39 -0300

    ya filtra y la barra está en el top pero no es fixed

--- data.js
+++ data.js
@@ -1,11 +1,23 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const example = () => {
-  return 'example';
-};
+//const example = () => {
+ // return 'example';
+//};
 
-window.example = example;
+//window.example = example;
 
 // funcion filtrar
+
+const filterType = (data,condition) => {
+  if (condition === "0") {
+    return data;
+  }
+  const pokeFilter = data.filter(element => {
+    if (element.type[0] === condition) return element.type[0] === condition;
+    else if (element.type[1] === condition) return element.type[1] === condition;
+  });
+  return pokeFilter;
+  };
+
 
